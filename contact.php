abdcb87c989f7a74d082e0e9ed639638294b6ae4
Polytechnical <43044551+Polytechnical@users.noreply.github.com>
2018-09-16 11:06:50 +0530

    true

--- contact.php
+++ contact.php
@@ -9,7 +9,7 @@ $email_body = "User Name: $name.\n".
 "User Email: $visitor_email.\n".
 "user message: $message.\n";
 
-$to = "anonymousp0646@gmail.com";
+$email_to = "anonymousp0646@gmail.com";
 $headers = "Form: $email_from \r\n";
 $headers = "Reply-To: $visitor_email \r\n";
 mail($to,$email_subject,$email_body,$headers);
