a964efe79a91a996eb068282817b129791194fa7
Polytechnical <43044551+Polytechnical@users.noreply.github.com>
2018-09-16 10:50:57 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,18 +1,18 @@
 <?php
-$name= $POST['email'];
-$visitor_email= $_POST['email'];
-$message= $_POST['message'];
+$name = $POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
 
-$email_from= 'https://polytechnical.github.io/contact/';
-$email_subject= "new form submission";
-$email_body= "user name: $name\n".
-"user email: $visitor_email.\n".
+$email_from = 'https://polytechnical.github.io/contact/';
+$email_subject = "New Form Submission";
+$email_body = "User Name: $name.\n".
+"User Email: $visitor_email.\n".
 "user message: $message.\n";
 
 $to = "anonymousp0646@gmail.com";
-$headers = "form: $email_from\r\n";
-$headers = "reply-to: $visitor_email \r\n";
+$headers = "Form: $email_from \r\n";
+$headers = "Reply-To: $visitor_email \r\n";
 mail($to,$email_subject,$email_body,$headers);
-header("location: index.html");
+header("Location: index.html");|
 
 ?>
